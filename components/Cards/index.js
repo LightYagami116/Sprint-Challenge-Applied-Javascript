1d83bf12efd567e87690c3b8299a455c49e7411f
unknown <wastefuldragon@gmail.com>
2020-05-15 22:52:44 -0400

    MVP finished

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,49 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+.then(res =>{
+    console.log(res)
+    const cardsComponent = document.querySelector('.cards-container')
+    const cardData = Object.values(res.data.articles)
+    // console.log(cardData)
+    cardData.forEach((articleSet)=>{
+        articleSet.forEach((article) =>{
+            cardsComponent.append(Cards(article))
+        })
+    })
+})
+.catch(error =>{ console.log(error); });
+
+
+function Cards(cardAtts) {
+    //create elements
+    const card = document.createElement('div');
+    const headline = document.createElement('div');
+    const author = document.createElement('div');
+    const imgCont = document.createElement('div');
+    const authImg = document.createElement('img');
+    const authName = document.createElement('span');
+
+    //append children to elements
+    card.appendChild(headline);
+    card.appendChild(author);
+    author.appendChild(imgCont);
+    imgCont.appendChild(authImg);
+    author.appendChild(authName);
+
+    //add classes to elements
+    card.classList.add('card');
+    headline.classList.add('headline');
+    author.classList.add('author');
+    imgCont.classList.add('img-container');
+
+    //add content
+    headline.textContent = cardAtts.headline;
+    authImg.src = cardAtts.authorPhoto;
+    authName.textContent = `By: ${cardAtts.authorName}`;
+
+
+    return card;
+}
